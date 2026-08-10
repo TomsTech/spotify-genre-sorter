85a92068bbc285bd8482c5194814e091603c7934
TomsTech <thomashouston2001@gmail.com>
2026-08-10 15:29:58 +1000

    chore: drop patch_comment.cjs from PR branch (out of scope)

--- patch_comment.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/routes/api.ts', 'utf8');
-
-code = code.replace(`    // Note: trackData has {id, artistIds} not {track: {id, artists: {id}[]}}
-    // So we inline the logic or adjust the helper. Let's create another helper or just leave it for now.\n`, '');
-
-fs.writeFileSync('src/routes/api.ts', code);
-console.log('Success');
